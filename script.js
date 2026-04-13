0e6f2b036a0268c14afc52c3a43e9bd6783c65ab
iamagithubuser101 <ramiradlv05@gmail.com>
2026-04-13 10:19:41 +0200

    Add upload function

--- script.js
+++ script.js
@@ -3,6 +3,33 @@ function likeMeme(button) {
     likes.textContent = parseInt(likes.textContent) + 1;
 }
 
+function uploadMeme(event) {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const container = document.querySelector(".memes");
+    var newMeme = document.createElement("div");
+    newMeme.classList.add("meme-container");
+
+    var img = document.createElement("img");
+    img.classList.add("meme-img");
+    img.src = URL.createObjectURL(file);
+
+    var close = document.createElement("span");
+    close.classList.add("close");
+    close.innerHTML = "&times;";
+
+    var likeButton = document.createElement("button");
+    likeButton.classList.add("lk_btn");
+    likeButton.innerHTML = "Like <span>0</span>";
+    likeButton.addEventListener("click", () => likeMeme(likeButton));
+
+    newMeme.appendChild(img);
+    newMeme.appendChild(close);
+    newMeme.appendChild(likeButton);
+    container.appendChild(newMeme);
+}
+
 const containers = document.querySelectorAll(".meme-container");
 
   containers.forEach(container => {
